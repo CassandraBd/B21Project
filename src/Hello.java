ca187c2974f4b0044748df8bb4452e5df970fc7f
CassandraBd <bodencassandra@gmail.com>
2020-11-10 11:31:25 -0600

    added ne hello

--- src/Hello.java
+++ src/Hello.java
@@ -1,10 +1,3 @@
 public class Hello {
-    public static void main(String[] args) {
-
-
-    // more stuff
-    // Mac user and window users are happy to see commit dialog finally
-        // help
-
-    }
+    // help
 }
